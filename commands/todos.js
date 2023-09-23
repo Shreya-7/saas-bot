fd3d07b0ba44ff2c91172fb06ef60f0d448659b2
Shreya Agarwal <a.shreya202@gmail.com>
2023-09-23 15:11:19 +0530

    refactor: extract out todo check functionality for reuse in multiple commands

--- commands/todos.js
+++ commands/todos.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder, escapeNumberedList } = require("discord.js");
+const { isMessageTodoDone, DONE_EMOJI } = require("../utils.js");
 
-const doneEmoji = '👍';
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('todos')
-        .setDescription('Lists all pending (not reacted with 👍) todo links from a channel (max 100)'),
+        .setDescription(`Lists all pending (not reacted with ${DONE_EMOJI}) todo links from a channel (max 100)`),
     async execute(interaction, channel) {
         var todos = [];
         var errorFound = false;
@@ -13,15 +13,7 @@ module.exports = {
                 console.log(`Received ${messages.size} messages`);
                 messages
                     .filter(message => (message.author.bot != true) && (message.content.includes('http')))
-                    .filter(message => {
-                        const messageReaction = message.reactions.resolve(doneEmoji);
-                        console.log(`Message "${message.content}" by ${message.author.username} has reactions=${messageReaction == null}`)
-                        if (messageReaction) {
-                            return messageReaction.count < 1;
-                        } else {
-                            return true;
-                        }
-                    })
+                    .filter(message => !isMessageTodoDone(message))
                     .forEach(message => todos.push([`- <${message.content}>`]));
             })
             .catch(error => {
---
+++ utils.js
@@ -0,0 +1,12 @@
+const DONE_EMOJI = '👍';
+
+function isMessageTodoDone(message) {
+    const messageReaction = message.reactions.resolve(DONE_EMOJI);
+    console.log(`Message "${message.content}" by ${message.author.username} has reactions=${messageReaction == null}`)
+    if (messageReaction) {
+        return messageReaction.count > 0;
+    }
+    return false;
+}
+
+module.exports = { isMessageTodoDone, DONE_EMOJI };
